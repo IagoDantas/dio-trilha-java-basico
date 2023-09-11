33ee9b5e1f631cc50636cb446132376d85dd4b11
Iago Dantas <iagodantas.8@gmail.com>
2023-09-11 10:03:29 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Tipos e variaveis");
+
+        // final representa como se fosse uma constante;
+        final double PI = 3.14;
+
     }
 }
